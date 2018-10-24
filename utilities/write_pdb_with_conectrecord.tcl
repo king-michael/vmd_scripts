0c440084acba5681ab0409f1f70b7e60bc82bd9f
Michael King <michael.king@uni-konstanz.de>
2018-10-24 14:12:09 +0200

    add write conect to pdb

---
+++ utilities/write_pdb_with_conectrecord.tcl
@@ -0,0 +1,38 @@
+
+# http://www.bmsc.washington.edu/CrystaLinks/man/pdb/part_69.html
+# COLUMNS         DATA TYPE        FIELD           DEFINITION
+#  1 -  6         Record name      "CONECT"
+#  7 - 11         Integer          serial          Atom serial number
+# 12 - 16         Integer          serial          Serial number of bonded atom
+# 17 - 21         Integer          serial          Serial number of bonded atom
+# 22 - 26         Integer          serial          Serial number of bonded atom
+# 27 - 31         Integer          serial          Serial number of bonded atom
+# 32 - 36         Integer          serial          Serial number of hydrogen bonded atom
+# 37 - 41         Integer          serial          Serial number of hydrogen bonded atom
+# 42 - 46         Integer          serial          Serial number of salt bridged atom
+# 47 - 51         Integer          serial          Serial number of hydrogen bonded atom
+# 52 - 56         Integer          serial          Serial number of hydrogen bonded atom
+# 57 - 61         Integer          serial          Serial number of salt bridged atom
+
+
+proc write_pdb_with_conect {fname {molid top}} {
+  # function to write the connect record for bonded atoms.
+  
+  # create pdb file
+  animate write pdb $fname $molid
+  
+  
+  set sel [atomselect $molid all]
+
+  set f [open $fname {WRONLY CREAT APPEND}] ;# instead of "a"
+  
+  foreach bond [$sel getbonds] i [$sel list] {
+    set str [format "%6s%5i" "CONECT" [incr i]]
+    foreach b $bond {
+    set str [format "$str%5i" [incr b]]
+    }
+    puts $f $str
+  }
+  
+  close $f
+}
