1ea1d1466b6cf85550c4016aaa182234abc48613
Michael King <michael.king@uni-konstanz.de>
2019-09-24 15:51:53 +0200

    add function to read user files

--- IO/README.md
+++ IO/README.md
@@ -4,3 +4,19 @@
 * `write_pdb_with_conect {fname {molid top}}` <br>
     Writes PDB with a CONECT record
     
+## `read_user_files.tcl`
+Functions to read files from users into VMD
+* `read_cluster_per_frame {filename {property user} {value 1} {molid top}}` <br>
+  Reads a file with atom ID per line, assigns for all atomID's in this line the `value` to `prorpery`. <br>
+  example: `read_cluster_per_frame ./atoms_in_cluster_frame-0_dp-0.32.dat user 1 top`
+  
+ * `read_file_to_atoms {filename args}` <br>
+  Reads a file with atom ID per line, assigns for all atomID's in this line the `value` to `prorpery`. <br>
+  example: `read_file_to_atoms ${pairfile} -stride $stride` <br>
+  Keywords
+    * `property` : property to store `value` in. Default is `user`
+    * `value` : value to store in `property`
+    * `molid` : molid of the used molecule
+    * `stride` : use a stride on the data
+       
+  
---
+++ IO/read_user_files.tcl
@@ -0,0 +1,63 @@
+# read in user data
+
+proc read_cluster_per_frame {filename {property user} {value 1} {molid top}} {
+  # read_cluster_per_frame ./atoms_in_cluster_frame-0_dp-0.32.dat user 1 top
+  set fp [open ${filename} "r"]
+  set sel [atomselect top all]
+
+  set ts 0
+  while { [gets ${fp} line] >= 0 } {
+    animate goto $ts
+    set data [$sel get $property]
+    foreach id $line {lset data $id $value}
+    $sel set $property ${data}
+    incr ts
+  }
+  close ${fp}
+  $sel delete
+}
+
+proc read_file_to_atoms {filename args} {
+  # read_file_to_atoms ${pairfile} -stride $stride
+
+  # START HANDLING KEYWORDS
+  array set settings {
+    property user
+    value       1
+    molid     top
+    stride      1
+  }
+  set index 0
+  for {set i 0} {$i < [llength $args]} {incr i} {
+    set arg [lindex $args $i]
+    if {[string match "-*" $arg]} {
+      set key [string trimleft $arg -]
+      incr i
+      if {[array names settings $key] != ""} {
+        set settings($key) [lindex $args $i]
+      } else {
+        set out "read_file_to_atoms filename"
+        foreach n [array names settings] {set out "$out -${n} $settings($n)"}
+        puts $out; return
+      }
+    }
+  }
+  # END HANDLING KEYWORDS
+
+  # START HANDLING FILE
+  set fp [open ${filename} "r"]
+  set sel [atomselect top all]
+  set ts -1
+  while { [gets ${fp} line] >= 0 } {
+    incr ts
+    if {[expr $ts % $settings(stride)] eq 0} {
+      animate goto [expr int($ts/$settings(stride))]
+      set data [$sel get $settings(property)]
+      foreach id $line {lset data $id $settings(value)}
+      $sel set $settings(property) ${data}
+    }
+  }
+  close ${fp}
+  $sel delete
+  # END HANDLING FILE
+}
